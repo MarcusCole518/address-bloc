fd47abd8e3ae08383bf8b6f9b810e3a10fdcdb88
Marcus Little <mcolelittle@gmail.com>
2018-12-18 01:17:56 -0700

    checkpoint-testing complete

--- controllers/MenuController.js
+++ controllers/MenuController.js
@@ -54,6 +54,9 @@ module.exports = class MenuController {
         this.main();
     }
 
+    getContactCount() {
+        return this.contacts.length;
+    }
 
     exit() {
         console.log('Thanks for using AddressBloc!');
