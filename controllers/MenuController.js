2f687cbeeb8a54d3f359869df0240b78f55f4e55
Marcus Little <mcolelittle@gmail.com>
2018-12-23 12:40:50 -0700

    checkpoint-listing-contacts completed

--- controllers/MenuController.js
+++ controllers/MenuController.js
@@ -8,7 +8,14 @@ module.exports = class MenuController {
                 type: 'list',
                 name: 'mainMenuChoice',
                 message: 'Please choose from an option below: ',
-                choices: ['Add new contact', 'Exit', 'Get Date', 'Remind Me']
+                choices: [
+                    'Add new contact',
+                    'View all contacts',
+                    'Exit',
+                    'Get Date',
+                    'Remind Me',
+                    'Search for a contact'
+                ]
             }
         ];
         this.book = new ContactController;
@@ -20,15 +27,21 @@ module.exports = class MenuController {
             switch(response.mainMenuChoice) {
                 case "Add new contact":
                     this.addContact();
-                    break;
+                    break
                 case "Exit":
                     this.exit();
                 case "Get Date":
                     this.getDate();
-                    break;
+                    break
                 case "Remind Me":
                     this.remindMe();
-                    break;
+                    break
+                case "View all contacts":
+                    this.getContacts();
+                    break
+                case "Search for a contact":
+                    this.search();
+                    break
                 default:
                     console.log("Invalid Input");
                     this.main();
@@ -79,5 +92,90 @@ module.exports = class MenuController {
     exit() {
         console.log('Thanks for using AddressBloc!');
         process.exit();
+    }
+
+    getContacts() {
+        this.clear();
+
+        this.book.getContacts().then((contacts) => {
+            for(let contact of contacts) {
+                console.log(`
+                name: ${contact.name}
+                phone number: ${contact.phone}
+                email: ${contact.email}
+                ---------------`
+                );
+            }
+            this.main();
+        }).catch((err) => {
+            console.log(err);
+            this.main();
+        })
+    }
+
+    search() {
+        inquirer.prompt(this.book.searchQuestions)
+        .then((target) => {
+            this.book.search(target.name)
+            .then((contact) => {
+                if(contact === null) {
+                    this.search();
+                } else {
+                    this.showContact(contact);
+                }
+            });
+        })
+        .catch((err) => {
+            console.log(err);
+            this.main();
+        });
+    }
+
+    showContact(contact) {
+        this._printContact(contact);
+        inquirer.prompt(this.book.showContactQuestions)
+        .then((answer) => {
+            switch(answer.selected) {
+                case "Delete contact":
+                    this.delete(contact);
+                    break;
+                case "Main menu":
+                    this.main();
+                    break;
+                default:
+                    console.log("Something went wrong.");
+                    this.showContact(contact);
+            }
+        })
+        .catch((err) => {
+            console.log(err);
+            this.showContact(contact);
+        })
+    }
+    _printContact(contact) {
+        console.log(`
+            name: ${contact.name}
+            phone number: ${contact.phone}
+            email: ${contact.email}
+            ----------------`
+        );
+    }
+
+    delete(contact) {
+        inquirer.prompt(this.book.deleteConfirmQuestions)
+        .then((answer) => {
+            if(answer.confirmation) {
+                this.book.delete(contact.id);
+                console.log("contact deleted!");
+                this.main();
+            } else {
+                console.log("contact not deleted");
+                this.showContact(contact);
+            }
+        })
+        .catch((err) => {
+            console.log(err);
+            this.main();
+        })
     }
 }
